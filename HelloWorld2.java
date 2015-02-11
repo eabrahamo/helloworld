0a52ac22de078c549eabad78a1e3e27f9bf49bbe
svc-corp-jenkins <svc-corp-jenkins@microstrategy.com>
2015-02-11 11:10:21 -0500

    Fix DE10 for demo

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -4,7 +4,7 @@ public class HelloWorld2 {
 
 	// TODO Auto-generated method stub #41
 		System.out.println("Hello World!");
-		System.out.println("Hello World5!")
+		System.out.println("Hello World3!")
 	}
 
 }
